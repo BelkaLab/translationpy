80c95426dd37eafe6010c9f7ade6fddbc5133972
Giovanni Frigo <john.frigo92@gmail.com>
2017-02-23 14:47:45 +0100

    Updating script
    Added multiple sheets support, improved arg parsing

--- translationpy.py
+++ translationpy.py
@@ -10,14 +10,12 @@ import sys
 from os import path, makedirs
 import shutil
 import codecs
+import argparse
 
-keys = []
-translations = []
 directory = 'export/'
-filename = ''
 #encoding = codecs.BOM_UTF16_BE
 
-def createfile(column):
+def createfile(column, keys, translations):
 	langCode = column[0].value
 	langName = column[1].value
 	if langName==None or langCode==None:
@@ -28,7 +26,7 @@ def createfile(column):
 
 	translations.append((langName,langCode))
 
-	print('Creating translation file for ' + langName + '\t(' + langCode + '.json)..')
+	print('  Creating translation file for {:10s}\t({:s}.json)..'.format(langName, langCode) )
 	f = open( directory + '/' + langCode + '.json', 'w')
 	#f.write(encoding)
 
@@ -50,31 +48,19 @@ def createfile(column):
 	if langCode == 'en':
 		shutil.copyfile( directory + '/en.json',  directory + '/lang.json' )
 
-if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		filename = sys.argv[1]
-	else:
-		print ("Usage: python excel_to_json.py filename.xlsx [export_directory]\n")
-		exit(-1)
-	if len(sys.argv) > 2:
-		directory = sys.argv[2]
-
-	if not path.exists(directory):
-		makedirs(directory)
-
-	wb = load_workbook( filename )
-	ws = wb.active #get active worksheet
-
-	for column in ws.columns:
+def processSheet(sheet):
+	keys = []
+	translations = []
+	for column in sheet.columns:
 		# generate the keys list
 		if keys.__len__() == 0:
 			for cell in column:
 				if cell.value is not None:
 					keys.append(cell.value)
 		else:
-			createfile(column)
+			createfile(column, keys, translations)
 
-	print( 'Writing languages.json.. ' )
+	print( 'Writing ' + directory + '/languages.json.. ' )
 	f = open( directory + '/languages.json', 'w' )
 	#f.write(encoding)
 	f.write( 'loadTranslationsList({\n' )
@@ -86,4 +72,34 @@ if __name__ == '__main__':
 			f.write( '\n' )
 	f.write( '});' )
 
-	print('Everything done.\n')
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Transform an excel file containing translations in the separate JSON files required by polyglot.js')
+	parser.add_argument('input', help='the input excel file')
+	parser.add_argument('--out-dir', help='specify the output directory (default: export/)', default='export')
+	parser.add_argument('--sheets', help='process only the specified sheets (default: active sheet, * for all)')
+
+	args = parser.parse_args()
+
+	wb = load_workbook( args.input )
+
+	if args.sheets:
+		# process all sheets matching given list, or all sheets if '*' was given
+		for sheetName in wb.get_sheet_names():
+			if args.sheets == '*' or sheetName in args.sheets:
+				print( '\nProcessing worksheet "%s"' % sheetName )
+
+				directory = args.out_dir + '/' + sheetName
+				if not path.exists(directory):
+					makedirs(directory)
+
+				processSheet(wb.get_sheet_by_name(sheetName))
+	else:
+		 # process only active worksheet
+		directory = args.out_dir
+		if not path.exists(directory):
+			makedirs(directory)
+
+		print('\nProcessing active worksheet "%s"' % wb.active.title)
+		processSheet(wb.active)
+
+	print('\nEverything done.\n')
